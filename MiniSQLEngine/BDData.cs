d1fe18d387058cc2b42cfb02f416e8108b0329a4
Xabi <xcillero001@ikasle.ehu.eus>
2019-02-11 13:32:30 +0100

    pruebas de diccionario

---
+++ MiniSQLEngine/BDData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine
+{
+    class BDData
+    {
+       static void Main (String[] args)
+        {
+            String[] lines = File.ReadAllLines("ejemploDatos.txt");
+            Dictionary<String, Int32> nameAges = new Dictionary<String, Int32>();
+            foreach (String nameAge in lines)
+            {
+                String[] split = nameAge.Split(',');
+                nameAges.Add(split[0], Convert.ToInt32(split[1]));
+            }
+        }
+
+       
+
+    }
+}
