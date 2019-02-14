b959969f0c25d0be97591b7011ed850b3093241f
Xabi <xcillero001@ikasle.ehu.eus>
2019-02-14 12:52:29 +0100

    testing engine

--- MiniSQLEngine/BDData.cs
+++ MiniSQLEngine/BDData.cs
@@ -4,22 +4,44 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace MiniSQLEngine
 {
-    class BDData
+    public class BDData
     {
        static void Main (String[] args)
         {
-            String[] lines = File.ReadAllLines("ejemploDatos.txt");
-            Dictionary<String, Int32> nameAges = new Dictionary<String, Int32>();
-            foreach (String nameAge in lines)
+           
+        }
+
+
+        public static void pruebaLectura()
+        {
+           
+            String[] lines = File.ReadAllLines("ejemploColumnas.txt");
+            List<List<dynamic>> outerList = new List<List<dynamic>>();
+            List<String> innerList1 = new List<string>();
+            List<int> innerList2 = new List<int>();
+            outerList.Add(innerList1);
+            foreach(String data in lines)
             {
-                String[] split = nameAge.Split(',');
-                nameAges.Add(split[0], Convert.ToInt32(split[1]));
+                String[] split = data.Split(',');
+                .Add(split[0], Convert.ToInt32(split[1]));
             }
         }
 
+        //public static void pruebaLectura()
+        //{
+        //    String[] lines = File.ReadAllLines("ejemploDatos.txt");
+        //    Dictionary<String, Int32> nameAges = new Dictionary<String, Int32>();
+        //    foreach (String nameAge in lines)
+        //    {
+        //        String[] split = nameAge.Split(',');
+        //        nameAges.Add(split[0], Convert.ToInt32(split[1]));
+
+        //    }
+        //}
        
 
     }
