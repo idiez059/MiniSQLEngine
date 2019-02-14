799419a99e1b6abd339c01cfc4241fd41da8a6c1
ismad <idiez059@ikasle.ehu.eus>
2019-02-14 12:51:09 +0100

    Merge?

--- testParsing/UnitTest1.cs
+++ testParsing/UnitTest1.cs
@@ -7,10 +7,53 @@ namespace testParsing
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSelect()
         {
-            //nuevo
-            //jajaja
+            String input1 = "SELECT * FROM alumno";
+            String input2 = "Select * FROM alumno WHERE age < 10";
+            String input3 = "SELECT FROM alumno";
+            String pattern = @"SELECT\s+(\*|\w+)\s+FROM\s+(\w+)(?:\s+WHERE\s+(\w+)\s+(\=|\<|\>)\s+(\w+))?(\;)
+";
+
+            Assert.IsTrue(System.Text.RegularExpressions.Regex.IsMatch(input1, pattern));
+            Assert.IsTrue(System.Text.RegularExpressions.Regex.IsMatch(input2, pattern));
+            Assert.IsFalse(System.Text.RegularExpressions.Regex.IsMatch(input3, pattern));
+
         }
+        public void TestDelete()
+        {
+            String input1 = "DELETE FROM alumno";
+            String input2 = "DELETE FROM alumno WHERE nombre = Xabi";
+            String input3 = "DELETE FROM ";
+            String pattern = @"DELETE\s+FROM\s+(\w+)(?:\s+WHERE\s+(\w+)\s+(\=|<|>)\s+(\w+))?(\;)";
+
+            Assert.IsTrue(System.Text.RegularExpressions.Regex.IsMatch(input1, pattern));
+            Assert.IsTrue(System.Text.RegularExpressions.Regex.IsMatch(input2, pattern));
+            Assert.IsFalse(System.Text.RegularExpressions.Regex.IsMatch(input3, pattern));
+
+        }
+        public void TestInsert()
+        {
+            String input1 = "INSERT INTO alumno";
+            String input2 = "INSERT INTO alumno WHERE nombre = Xabi";
+            String input3 = "INSERT FROM alumno WHERE nombre = Xabi ";
+            String pattern = @"INSERT INTO\s+(\*|\w+)(?:\s+[WHERE\s+(\w+)\s+(\=|\<|\>)\s+(\w+)]+)?(\;)";
+
+            Assert.IsTrue(System.Text.RegularExpressions.Regex.IsMatch(input1, pattern));
+            Assert.IsTrue(System.Text.RegularExpressions.Regex.IsMatch(input2, pattern));
+            Assert.IsFalse(System.Text.RegularExpressions.Regex.IsMatch(input3, pattern));
+
+        }
+        public void TestUpdate()
+        {
+            String input1 = "UPDATE alumno SET nombre = Nadia WHERE nombre = Xabi";
+            String input3 = "UPDATE alumno SET nombre = Nadia nombre = Xabi";
+            String pattern = @"UPDATE\s+(\w+)\s+SET\s+(\w+)\s+=\s+(\w+)(?:\s+WHERE\s+(\w+)\s+(\=|\<|\>)\s+(\w+))?(\;)";
+
+            Assert.IsTrue(System.Text.RegularExpressions.Regex.IsMatch(input1, pattern));
+            Assert.IsFalse(System.Text.RegularExpressions.Regex.IsMatch(input3, pattern));
+
+        }
+
     }
 }
