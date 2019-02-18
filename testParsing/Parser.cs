7dcd802037f14506b41e875c8de80cd51958ec66
Nadia <nreina003@ikasle.ehu.eus>
2019-02-18 12:23:03 +0100

    who are we going to plan the work?¿?¿?¿? COMENTS

--- testParsing/Parser.cs
+++ testParsing/Parser.cs
@@ -4,5 +4,16 @@ public class Class1
 {
 	public Class1()
 	{
+        //recogemos la sentencia que metamos 
+        
+
+        //reconocer el tipo de sentencia (select por ahora)
+
+
+
+        //cuando sabe cual es, va a ala clase correspondiente (select en este caso)
+
+
+        regex
 	}
 }
--- testParsing/Select.cs
+++ testParsing/Select.cs
@@ -4,6 +4,8 @@ public abstract class Class1
 {
 	abstract public Class1()
 	{
+        //hacer un metodo parsear
+        //poner los parametros 
 
 	}
 }
