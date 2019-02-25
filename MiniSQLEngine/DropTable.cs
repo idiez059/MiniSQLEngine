22bb7fa28aedd62234a1168f637239f7b640b3a0
ismad <idiez059@ikasle.ehu.eus>
2019-02-25 13:07:38 +0100

    Classes added to solution

---
+++ MiniSQLEngine/DropTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class DropTable
+{
+	public DropTable(String query)
+    {
+        string dropTable = @"DROP TABLE\s+(\*|\w+)(\;)";
+        String[] partes = Regex.Split(query, dropTable); 
+	
+	}
+}
