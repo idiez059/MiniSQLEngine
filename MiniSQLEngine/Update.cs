f14fda7875a263676f76ab0c22d2b29f37a32cf8
ismad <idiez059@ikasle.ehu.eus>
2019-03-07 12:44:20 +0100

    Removed unused code

--- MiniSQLEngine/Update.cs
+++ MiniSQLEngine/Update.cs
@@ -3,14 +3,7 @@ using System.Text.RegularExpressions;
 
 public class Update : Query
 {
-    /*
-    public Update(String query)
-    {
-        string update = @"UPDATE\s+(\w+)\s+SET\s+(\w+)\s+=\s+(\w+)(?:\s+WHERE\s+(\w+)\s+(\=|\<|\>)\s+(\w+))?(\;)";
-        String[] partes = Regex.Split(query, update);
-    }
-    */
-
+   
     String pColumns;
     String pTabla;
     String pContenido;
