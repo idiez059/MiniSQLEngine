7e705063c418215a474eaaf8b8b4bc536f8afecb
ismad <idiez059@ikasle.ehu.eus>
2019-03-07 12:14:28 +0100

    .

--- MiniSQLEngine/FileSystemAbstract.cs
+++ MiniSQLEngine/FileSystemAbstract.cs
@@ -11,6 +11,16 @@ namespace MiniSQLEngine
 
     class FileSystemAbstract
     {
+        static void Main()
 
-    }
+        // Get the files in the directory and print out some information about them.
+        System.IO.FileInfo[] fileNames = dirInfo.GetFiles("*.*");
+
+
+        foreach (System.IO.FileInfo fi in fileNames)
+        {
+            Console.WriteLine("{0}: {1}: {2}", fi.Name, fi.LastAccessTime, fi.Length);
+        }
+
+}
 }
