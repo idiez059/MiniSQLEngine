4fdf88b136187bbaee87b4ef06e41f5f2e1e1782
Iñaki Zorrilla García <izorrilla008@ikasle.ehu.eus>
2019-03-26 10:24:20 +0100

    handling 'where' in select, correcting parameter entrance from parser

--- MiniSQLEngine/Select.cs
+++ MiniSQLEngine/Select.cs
@@ -23,27 +23,113 @@ public class Select : Query
         //Get the table
         Table table = db.GetTableByName(TableName);
         if (table == null) return Messages.TableDoesNotExist;
+        //List<Column> selectedColumns = new List<Column>();
 
-        List<Column> selectedColumns = new List<Column>();
 
-        if (ColumnNames.Count == 0)
-            return Messages.WrongSyntax;
-        else if (ColumnNames.Count == 1 && ColumnNames[0] == "*")
-        {
-            //SELECT *
-            return db.SelectAll(TableName).ToString();
+        if(Condition == null)
+        {        
+            if (ColumnNames.Count == 0)
+                return Messages.WrongSyntax;
+            else if (ColumnNames.Count == 1 && ColumnNames[0] == "*")
+            {
+                //SELECT *
+                return db.SelectAll(TableName).ToString();
+            }
+            else
+            {
+                //SELECT Name,Age,...
+                try
+                {
+                    return db.SelectColumns(TableName, ColumnNames).ToString();
+                }
+                catch
+                {
+                    return Messages.ColumnDoesNotExist;
+                }
+            }
         }
         else
         {
-            //SELECT Name,Age,...
-            try
+            if (ColumnNames.Count == 0)
+                return Messages.WrongSyntax;
+            else if (ColumnNames.Count == 1 && ColumnNames[0] == "*")
             {
-                return db.SelectColumns(TableName, ColumnNames).ToString();
+                //SELECT *
+                return db.SelectAll(TableName).ToString();
             }
-            catch
+            else
             {
-                return Messages.ColumnDoesNotExist;
+                //SELECT Name,Age,... & condition
+                string[] splitCond = Condition.Split(' ');
+                Column condColumn = table.ColumnByName(splitCond[0]);
+                string condColumnName = splitCond[0].Trim();
+                string condSymbol = splitCond[1].Trim();
+                string condValue = splitCond[2].Trim();
+                
+                //Handle condition
+
+
+                //if (condSymbol == "=")
+                //{
+                //    //condTable = db.SelectColumns(TableName, ColumnNames);
+                //    List<Column> selectedCondColumns = new List<Column>();
+                //    int colValueNum = table.ColumnByName(condColumnName).
+                //        GetNumValues();
+                //    for (int i=0; colValueNum > i; i++)
+                //    {
+                //        string gottenValue =
+                //        table.ColumnByName(condColumnName).GetValueAsString(i);
+
+                //        if(gottenValue == condValue)
+                //        {
+                            
+                //            selectedCondColumns.Add(toAdd);
+                //            Table condTable = new Table(TableName, selectedColumns);
+                //            return condTable.ToString();
+                //        }
+                //    }
+
+                //}
+                //else if (condSymbol == "<")
+                //{
+
+                //}
+                //else if (condSymbol == ">")
+                //{
+
+                //}
+                //else
+                //{
+                //    return Messages.ConditionDoesNotExist;
+                //}
+                try
+                {
+                    return db.SelectColumns(TableName, ColumnNames).ToString();
+                }
+                catch
+                {
+                    return Messages.ColumnDoesNotExist;
+                }
             }
+        }
+    }
+    private bool CompareOp(string elem1, string elem2, string operat)
+    {
+        switch (operat)
+        {
+            case "=":
+                return elem1 == elem2;
+            case "<":
+                int j = string.Compare(elem1, elem2);
+                if(j == -1) {return true;}
+                else { return false; }
+            case ">":
+                int k = string.Compare(elem1, elem2);
+                if (k == 1) { return true; }
+                else { return false; }
+            default:
+                System.Console.WriteLine("Error comparing tuples, in Compare() method");
+                return false;
         }
     }
 }
