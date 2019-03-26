0060baa115483ce270548f78ebeee86b600c3dbf
Iñaki Zorrilla García <izorrilla008@ikasle.ehu.eus>
2019-03-26 19:12:29 +0100

    Finished 'where' from select. Must verify its ok. (I doubt it is ok...)

--- MiniSQLEngine/Select.cs
+++ MiniSQLEngine/Select.cs
@@ -7,8 +7,10 @@ public class Select : Query
 {
     List<string> ColumnNames = new List<string>();
     String TableName { get; }
-    String Condition;
-    public Select(string columns, string table, string condition)
+    String ConditionCol;
+    String ConditionOp;
+    String ConditionValue;
+    public Select(string columns, string table, string condCol, string condOp, string condValue)
 	{
         //parse the list of columns
         string [] columnNames = columns.Split(',');
@@ -16,7 +18,9 @@ public class Select : Query
             ColumnNames.Add(columnName.Trim()); //Trim() removes spaces from the start and end
 
         TableName = table;
-        this.Condition = condition;
+        this.ConditionCol = condCol;
+        this.ConditionOp = condOp;
+        this.ConditionValue = condValue;
 	}
     public override String Run(Database db)
     {
@@ -26,7 +30,7 @@ public class Select : Query
         //List<Column> selectedColumns = new List<Column>();
 
 
-        if(Condition == null)
+        if(ConditionOp == null)
         {        
             if (ColumnNames.Count == 0)
                 return Messages.WrongSyntax;
@@ -60,48 +64,21 @@ public class Select : Query
             else
             {
                 //SELECT Name,Age,... & condition
-                string[] splitCond = Condition.Split(' ');
-                Column condColumn = table.ColumnByName(splitCond[0]);
-                string condColumnName = splitCond[0].Trim();
-                string condSymbol = splitCond[1].Trim();
-                string condValue = splitCond[2].Trim();
-                
                 //Handle condition
+                int numValues = table.ColumnByName(ConditionCol).GetNumValues();
+                for (int i=0; i<numValues; i++) //esto hay que probarlo
+                {
+                    string gottenValue =
+                        table.ColumnByName(ConditionCol).GetValueAsString(i);
+                    bool comparation = CompareOp(gottenValue, ConditionValue, ConditionOp);
+                    if (comparation == false)
+                    {
+                        table.Columns.RemoveAt(i);  //preguntar en clase
+                                                    //no estoy seguro
+                    }
+                    return table.ToString();
+                }
 
-
-                //if (condSymbol == "=")
-                //{
-                //    //condTable = db.SelectColumns(TableName, ColumnNames);
-                //    List<Column> selectedCondColumns = new List<Column>();
-                //    int colValueNum = table.ColumnByName(condColumnName).
-                //        GetNumValues();
-                //    for (int i=0; colValueNum > i; i++)
-                //    {
-                //        string gottenValue =
-                //        table.ColumnByName(condColumnName).GetValueAsString(i);
-
-                //        if(gottenValue == condValue)
-                //        {
-                            
-                //            selectedCondColumns.Add(toAdd);
-                //            Table condTable = new Table(TableName, selectedColumns);
-                //            return condTable.ToString();
-                //        }
-                //    }
-
-                //}
-                //else if (condSymbol == "<")
-                //{
-
-                //}
-                //else if (condSymbol == ">")
-                //{
-
-                //}
-                //else
-                //{
-                //    return Messages.ConditionDoesNotExist;
-                //}
                 try
                 {
                     return db.SelectColumns(TableName, ColumnNames).ToString();
