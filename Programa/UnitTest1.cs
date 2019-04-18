a54be4d6241e57df33fd6fd0c881d40cb2dcd2c3
Nadia <nreina003@ikasle.ehu.eus>
2019-04-18 11:30:46 +0200

    Select test, 1 or 2 options in the where

--- Programa/UnitTest1.cs
+++ Programa/UnitTest1.cs
@@ -12,13 +12,56 @@ namespace Programa
         {
             Database db = new Database("test-db");
             db.RunQuery("CREATE TABLE People (Name TEXT, Email TEXT, Age INT);");
+
+            db.RunQuery("INSERT INTO People VALUES ('Nerea', 'nerea@gmail.com', 17);");
+            db.RunQuery("INSERT INTO People VALUES ('Jon', 'jon@gmail.com', 18);");
+            db.RunQuery("INSERT INTO People VALUES ('Nuria', 'nuria@gmail.com', 20);");
             db.RunQuery("INSERT INTO People VALUES ('Rafa', 'rafa@gmail.com', 23);");
-            //Este test lo pasa
-            string result1 = db.RunQuery("SELECT Name FROM People WHERE Age = 23;"); 
+            db.RunQuery("INSERT INTO People VALUES ('Maria', 'maria@gmail.com', 25);");
+            db.RunQuery("INSERT INTO People VALUES ('Arrate', 'arrate@gmail.com', 25);");
+            db.RunQuery("INSERT INTO People VALUES ('Luis', 'luis@gmail.com', 27);");
+            db.RunQuery("INSERT INTO People VALUES ('Luisma', 'luisma@gmail.com', 28);");
+
+            string result1 = db.RunQuery("SELECT Name FROM People WHERE Age = 23;");
+            string result2 = db.RunQuery("SELECT Name FROM People WHERE Age > 21;");
+            string result3 = db.RunQuery("SELECT Name FROM People WHERE Age < 19;");
+            string result4 = db.RunQuery("SELECT Name FROM People WHERE Age < 23;");
+            string result5 = db.RunQuery("SELECT Name FROM People WHERE Age < 18;");
+            string result6 = db.RunQuery("SELECT Name FROM People WHERE Age = 25;");
+            string result7 = db.RunQuery("SELECT Name FROM People WHERE Age > 26;");
+            string resultn = db.RunQuery("SELECT Name FROM People;");
+
+            ////-------------Test funcionalesdel con 1 solo where ----------
+
+            //// = ---- FUNCIONA
             Assert.AreEqual("{Name,Email,Age}{'Rafa','rafa@gmail.com',23}", result1);
-            Assert.AreNotEqual("{Name,Email,Age}{'Luis', 'rafa@gmail.com', 23}", result1);
-            // Este test no pasa
-            //string result = db.RunQuery("SELECT Name FROM People;");
+
+            ////> ----FUNCIONA
+            Assert.AreEqual("{Name,Email,Age}{'Rafa','rafa@gmail.com',23}", result2);
+
+            ////< -----------(NO FUNCIONA, DICE QUE NO RECIBE NADA)
+
+            //Assert.AreEqual("{Name,Email,Age}{'Jon','jon@gmail.com',18}", result3);
+
+            //// sin where ------ NO FUNCIONA, DICE QUE esperaba {Name})
+
+            //Assert.AreEqual("{Name,Email,Age}{'Rafa','rafa@gmail.com',23}{'Jon','jon@gmail.com',18}{'Nuria','nuria@gmail.com',20}", resultn);
+
+
+            ////-------------Test funcionales con 2 where ----------
+            //// = ---FUNCIONA
+            Assert.AreEqual("{Name,Email,Age}{'Maria','maria@gmail.com',25}{'Arrate','arrate@gmail.com',25}", result6);
+            //<  ---FUNCIONA
+            Assert.AreEqual("{Name,Email,Age}{'Nerea','nerea@gmail.com',17}{'Jon','jon@gmail.com',18}", result3);
+            //> ---FUNCIONA
+            Assert.AreEqual("{Name,Email,Age}{'Luis','luis@gmail.com',27}{'Luisma','luisma@gmail.com',28}", result7);
+
+            //    ////-------------Test funcionales con 3 where ----------
+            //    //Assert.AreEqual("{Name,Email,Age}{'Rafa','rafa@gmail.com',23}{'Maria','maria@gmail.com',23}{'Luis','luis@gmail.com',23}", result1);
+
+           
+
+
 
         }
 
