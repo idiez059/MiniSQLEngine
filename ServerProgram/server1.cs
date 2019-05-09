af4a6a1cf7125d147f5ce5557b7118624f733954
Iñaki Zorrilla García <izorrilla008@ikasle.ehu.eus>
2019-05-09 13:16:39 +0200

    server opens a db or runs a query. unfinished

---
+++ ServerProgram/server1.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+using MiniSQLEngine;
+
+namespace ServerProgram
+{
+    public class server1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("JINIX DB Server v0.000000001");
+            const string argPrefixPort = "port=";
+
+            int port = 0;
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith(argPrefixPort)) port = int.Parse(arg.Substring(argPrefixPort.Length));
+            }
+            if (port == 0)
+            {
+                Console.WriteLine("ERROR. Usage: TCPClient ip=<ip> port=<port>");
+                return;
+            }
+
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            Console.WriteLine("Server listening for clients");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connection accepted");
+
+                var childSocketThread = new Thread(() =>
+                {
+                    byte[] inputBuffer = new byte[1024];
+
+                    NetworkStream networkStream = client.GetStream();
+
+                    //Read message from the client
+                    int size = networkStream.Read(inputBuffer, 0, 1024);
+                    string request = Encoding.ASCII.GetString(inputBuffer, 0, size);
+
+                    while (request != "END") //doing thingies here
+                    {
+                        Database db = null;
+                        string theAnswer = "";
+                        Match openADb = Regex.Match(things, things);
+                        System.Text.RegularExpressions.Match runAQuery = Regex.Match(things, things);
+
+                        if (openADb.Success)
+                        {
+                            db = new Database(openADb.Groups[1].Value,openADb.Groups[2].Value,openADb.Groups[3].Value);
+
+
+
+
+                        }else if (runAQuery.Success)
+                        {
+
+
+
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Could not understand what to do!");
+                            theAnswer = "<Error> Database, user or password are incorrect </Error>";
+                        }
+
+
+                        Console.WriteLine("Request received: " + request);
+
+                        byte[] outputBuffer = Encoding.ASCII.GetBytes("My answer is NO");
+                        networkStream.Write(outputBuffer, 0, outputBuffer.Length);
+
+                        size = networkStream.Read(inputBuffer, 0, 1024);
+                        request = Encoding.ASCII.GetString(inputBuffer, 0, size);
+                    }
+                    client.Close();
+                });
+                childSocketThread.Start();
+            }
+        }
+    }
+}
