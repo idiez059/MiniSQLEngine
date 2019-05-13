c8c5adf0cf14b76f41a39afb91df6ffe344b0493
Nadia <nreina003@ikasle.ehu.eus>
2019-05-13 13:06:40 +0200

    ConsoleClient finishhh

--- consoleClient/Program.cs
+++ consoleClient/Program.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace consoleClient
 {
@@ -13,9 +16,9 @@ namespace consoleClient
         private string user;
         private string pass;
         private string ip;
-        private string port;
+        private int port;
        
-        public Program(string datab, string user, string pass,string ip, string port)
+        public Program(string datab, string user, string pass,string ip, int port)
         {
             this.datab = datab;
             this.user = user;
@@ -29,7 +32,7 @@ namespace consoleClient
             Console.Write("Enter the IP: ");
             string ip = Console.ReadLine();
             Console.Write("Enter the port: ");
-            string port = Console.ReadLine();
+            int port = int.Parse(Console.ReadLine());
             Console.Write("Enter your user: ");
             string user = Console.ReadLine();
             Console.Write("Enter your password: ");
@@ -37,29 +40,45 @@ namespace consoleClient
             Console.Write("Enter the name of the database: ");
             string datab = Console.ReadLine();
 
-            Program db = new Program(datab, user, pass,ip,port);
-            //if (Program.init(datab, user, pass) == Constants.OpenDatabaseSuccess)
-            //{
-            //    db = new Program(datab, user, pass);
-            //    Console.WriteLine("Database opened");
-            //}
-            //i--;
-
             
-
-
-            Console.WriteLine("Write exit when you want to finish");
-            string query;
-            Console.Write("Enter the query: ");
-            query = Console.ReadLine();
-
-            while (query.ToLower() != "exit")
+            if (ip == null || port == 0 )
             {
-                Console.Write("Enter the query: ");
-                query = Console.ReadLine();
-                
+                Console.WriteLine("ERROR. Usage: TCPClient ip=<ip> port=<port>");
+                return;
             }
-            Environment.Exit(0);
+                      
+            using (TcpClient client = new TcpClient(ip, port))
+            {
+                NetworkStream networkStream = client.GetStream();
+
+                byte[] database = Encoding.ASCII.GetBytes(datab);
+                byte[] userBuffer = Encoding.ASCII.GetBytes(user);
+                byte[] passBuffer = Encoding.ASCII.GetBytes(pass);
+                byte[] inputBuffer = new byte[1024];
+
+                networkStream.Write(database,0,database.Length);
+                int readBytes = networkStream.Read(inputBuffer, 0, 1024);
+                Console.WriteLine("Server response to database: " + readBytes);
+                networkStream.Write(userBuffer, 0, userBuffer.Length);
+                readBytes = networkStream.Read(inputBuffer, 0, 1024);
+                Console.WriteLine("Server response to user: " + readBytes);
+                networkStream.Write(passBuffer, 0, passBuffer.Length);
+                readBytes = networkStream.Read(inputBuffer, 0, 1024);
+                Console.WriteLine("Server response to password: " + readBytes);
+
+                Console.WriteLine("Write exit when you want to finish");
+                string query;
+                byte[] queryBuffer;
+                do
+                {
+                    Console.Write("Enter the query: ");
+                    query = Console.ReadLine();
+                    queryBuffer = Encoding.ASCII.GetBytes(query);
+                    networkStream.Write(queryBuffer, 0, queryBuffer.Length);
+                    readBytes = networkStream.Read(inputBuffer, 0, 1024);
+                    Console.WriteLine("Server response to query: " + query + "; " + readBytes);
+                } while (query.ToLower() != "exit");
+            }
         }
     }
 }
